654240f24fe4b1a32901f8a53d129f9894454794
DarkMG73 <levelthreeemail@gmail.com>
2024-04-06 17:17:19 -0400

    2024-04-06T21:17:19.005Z:

--- src/Components/User/Register/Register.jsx
+++ src/Components/User/Register/Register.jsx
@@ -76,14 +76,14 @@ const Register = (props) => {
         // If not period, ask if that was intended
         if (!inputNameObject[key].includes(".")) {
           const confirmPeriod = window.confirm(
-            'There does not appear to be period "." in the email address. If there is no period in this email address, click "OK" to submit the form. \n\nMost email addresses have a period and a suffix like ".com" or ".net". If this email address should have a period, then click "CANCEL" to return to the registration form'
+            'There does not appear to be period "." in the email address. If there is no period in this email address, click "OK" to submit the form. \n\nMost email addresses have a period and a suffix like ".com" or ".net". If this email address should have a period, then click "CANCEL" to return to the registration form',
           );
           if (!confirmPeriod) return;
         }
       } else if (key === "password") {
         const passwordValidCheck = passwordValidator(
           inputNameObject[key],
-          true
+          true,
         );
 
         if (!passwordValidCheck.isValid) {
@@ -98,7 +98,7 @@ const Register = (props) => {
                   return "   " + (i + 1) + ": " + groomedMessage + ". ";
                 })
                 .join(
-                  "\n"
+                  "\n",
                 )}\n\nHere are all of the password requirements: ${passwordRequirements}`,
             };
         }
@@ -137,9 +137,16 @@ const Register = (props) => {
       if (inputsValidCheck.valid) {
         // axios("http://localhost:8000/api/users/auth/register", user)
         registerAUser(user).then((res) => {
+          console.log(
+            "%c⚪️►►►► %cline:139%cres",
+            "color:#fff;background:#ee6f57;padding:3px;border-radius:2px",
+            "color:#fff;background:#1f3c88;padding:3px;border-radius:2px",
+            "color:#fff;background:rgb(89, 61, 67);padding:3px;border-radius:2px",
+            res,
+          );
           if (res && res.status >= 400) {
             alert(
-              `There was an error trying to complete the registration process. ${res.message}`
+              `There was an error trying to complete the registration process. ${res.message}`,
             );
           }
           if (res && res.status < 400) {
@@ -153,12 +160,12 @@ const Register = (props) => {
                       "There was a problem finding the user database. Make sure you are connected to the internet. Contact the site admin if the problem continues. Error: " +
                         res.status +
                         " | " +
-                        res.statusText
+                        res.statusText,
                     );
                     setShowLoginError(true);
                   } else if (res.status >= 400) {
                     setLoginError(
-                      res.data.message ? res.data.message : res.statusText
+                      res.data.message ? res.data.message : res.statusText,
                     );
                     setShowLoginError(true);
                   }
@@ -167,7 +174,7 @@ const Register = (props) => {
                   dispatch(authActions.logIn(res.data));
                 } else {
                   setLoginError(
-                    "Unfortunately, something went wrong and we can not figure out what happened.  Please refresh and try again."
+                    "Unfortunately, something went wrong and we can not figure out what happened.  Please refresh and try again.",
                   );
                   setShowLoginError(true);
                 }
@@ -185,7 +192,7 @@ const Register = (props) => {
       }
     } else {
       alert(
-        'The CAPTCHA test has failed. Please fix the wrong items and then resubmit. Keep in mind that "I" and "1" and "l" can look really similar in that test. "0", "O" and "o" can also be mistaken.'
+        'The CAPTCHA test has failed. Please fix the wrong items and then resubmit. Keep in mind that "I" and "1" and "l" can look really similar in that test. "0", "O" and "o" can also be mistaken.',
       );
     }
   };
