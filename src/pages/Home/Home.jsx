6daa60df9f997e77d2bd9a06023c619bcdc64b58
DarkMG73 <levelthreeemail@gmail.com>
2024-04-08 12:27:06 -0400

    2024-04-08T16:27:05.932Z:

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -45,6 +45,9 @@ const Home = (props) => {
   ////////////////////////////////////////
   useEffect(() => {
     dispatch(loadingRequestsActions.addToLoadRequest());
+    setTimeout(() => {
+      dispatch(loadingRequestsActions.removeFromLoadRequest());
+    }, 3000);
   }, []);
 
   useLayoutEffect(() => {
@@ -389,16 +392,22 @@ const Home = (props) => {
 
       {props.isDemo && studyPlan && Object.keys(studyPlan).length <= 0 && (
         <div className={styles["demo-error-modal"]}>
-          <h2>There seems to be a problem loading the demo data.</h2>
-          <p>
-            We are very sorry for the trouble. This is most likely to be caused
-            by a network issue or backend server issue. Please refresh and try
-            again. If the problem continues, please email{" "}
-            <a href="mailto:general@glassinteractive.com">
-              general@glassinteractive.com
-            </a>
-            . We will get it figured out right away.
-          </p>
+          {setTimeout(() => {
+            return (
+              <Fragment>
+                <h2>There seems to be a problem loading the demo data.</h2>
+                <p>
+                  We are very sorry for the trouble. This is most likely to be
+                  caused by a network issue or backend server issue. Please
+                  refresh and try again. If the problem continues, please email{" "}
+                  <a href="mailto:general@glassinteractive.com">
+                    general@glassinteractive.com
+                  </a>
+                  . We will get it figured out right away.
+                </p>
+              </Fragment>
+            );
+          }, 2000)}
         </div>
       )}
     </div>
