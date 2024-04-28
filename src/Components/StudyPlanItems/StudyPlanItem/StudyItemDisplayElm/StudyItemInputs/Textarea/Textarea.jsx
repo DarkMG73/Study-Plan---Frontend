3e022fc54288168510c6622c07afa79594090ac7
DarkMG73 <levelthreeemail@gmail.com>
2024-04-28 11:37:47 -0400

    Now has div for textarea to size itself on load and TextareaAutosize in edit mode.

--- src/Components/StudyPlanItems/StudyPlanItem/StudyItemDisplayElm/StudyItemInputs/Textarea/Textarea.jsx
+++ src/Components/StudyPlanItems/StudyPlanItem/StudyItemDisplayElm/StudyItemInputs/Textarea/Textarea.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect, Fragment } from "react";
 import Styles from "./Textarea.module.scss";
 import useAddInputData from "../../../../../../Hooks/useAddInputData";
-// import TextareaAutosize from "react-textarea-autosize";
+import TextareaAutosize from "react-textarea-autosize";
 
 const Textarea = (props) => {
   const {
@@ -28,38 +28,7 @@ const Textarea = (props) => {
     };
 
   const onBlurHandler = (e) => {
-    console.log(
-      "%c⚪️►►►► %cline:30%ce",
-      "color:#fff;background:#ee6f57;padding:3px;border-radius:2px",
-      "color:#fff;background:#1f3c88;padding:3px;border-radius:2px",
-      "color:#fff;background:rgb(1, 77, 103);padding:3px;border-radius:2px",
-      e.nativeEvent.target,
-    );
-    console.log(
-      "%c⚪️►►►► %cline:30%ce",
-      "color:#fff;background:#ee6f57;padding:3px;border-radius:2px",
-      "color:#fff;background:#1f3c88;padding:3px;border-radius:2px",
-      "color:#fff;background:rgb(1, 77, 103);padding:3px;border-radius:2px",
-      e.nativeEvent.target.innerHTML,
-    );
-    // Groom for standard input event
-    const newE = e.nativeEvent;
-    newE.target.value = e.nativeEvent.target.innerHTML;
-    console.log(
-      "%c⚪️►►►► %cline:47%cnewE",
-      "color:#fff;background:#ee6f57;padding:3px;border-radius:2px",
-      "color:#fff;background:#1f3c88;padding:3px;border-radius:2px",
-      "color:#fff;background:rgb(20, 68, 106);padding:3px;border-radius:2px",
-      newE.target,
-    );
-    console.log(
-      "%c⚪️►►►► %cline:47%cnewE",
-      "color:#fff;background:#ee6f57;padding:3px;border-radius:2px",
-      "color:#fff;background:#1f3c88;padding:3px;border-radius:2px",
-      "color:#fff;background:rgb(20, 68, 106);padding:3px;border-radius:2px",
-      newE.target.value,
-    );
-    addInputData(newE, { emptyForm, editedField, setEditedField });
+    addInputData(e, { emptyForm, editedField, setEditedField });
   };
 
   useEffect(() => {
@@ -90,34 +59,59 @@ const Textarea = (props) => {
       >
         {key}:
       </label>
-      <div
-        id={
-          parentMasterID +
-          "-" +
-          parentsParentKey +
-          "-" +
-          parentKey +
-          "-" +
-          key +
-          "textarea"
-        }
-        key={parentKey + "-" + key}
-        name={parentKey + "-" + key}
-        data-category={studyPlanItemsObj[key]}
-        placeholder={""}
-        title={key}
-        data-parentkey={parentKey}
-        data-parentsparentkey={parentsParentKey ? parentsParentKey : ""}
-        data-parentmasterid={parentMasterID}
-        onChange={onChangeNotes()}
-        onBlur={onBlurHandler}
-        defaultValue={notes}
-        contentEditable={unlockProtectedVisible.includes(parentMasterID)}
-        suppressContentEditableWarning={true}
-        className="sizable-textarea"
-      >
-        {notes}
-      </div>
+      {unlockProtectedVisible.includes(parentMasterID) && (
+        <TextareaAutosize
+          id={
+            parentMasterID +
+            "-" +
+            parentsParentKey +
+            "-" +
+            parentKey +
+            "-" +
+            key +
+            "textarea"
+          }
+          key={parentKey + "-" + key}
+          name={parentKey + "-" + key}
+          data-category={studyPlanItemsObj[key]}
+          placeholder={""}
+          title={key}
+          data-parentkey={parentKey}
+          data-parentsparentkey={parentsParentKey ? parentsParentKey : ""}
+          data-parentmasterid={parentMasterID}
+          onChange={onChangeNotes()}
+          onBlur={onBlurHandler}
+          defaultValue={notes}
+          contentEditable={unlockProtectedVisible.includes(parentMasterID)}
+          suppressContentEditableWarning={true}
+          className="sizable-textarea"
+        />
+      )}
+      {!unlockProtectedVisible.includes(parentMasterID) && (
+        <div
+          id={
+            parentMasterID +
+            "-" +
+            parentsParentKey +
+            "-" +
+            parentKey +
+            "-" +
+            key +
+            "textarea"
+          }
+          key={parentKey + "-" + key}
+          name={parentKey + "-" + key}
+          data-category={studyPlanItemsObj[key]}
+          placeholder={""}
+          title={key}
+          data-parentkey={parentKey}
+          data-parentsparentkey={parentsParentKey ? parentsParentKey : ""}
+          data-parentmasterid={parentMasterID}
+          className="sizable-textarea"
+        >
+          {notes}
+        </div>
+      )}
     </Fragment>
   );
 };
